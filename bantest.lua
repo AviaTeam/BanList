6b9a3002cc67ed44c606a15e7fe0a6f53a918905
AviaTeam <76924672+AviaTeam@users.noreply.github.com>
2022-08-11 12:15:31 +0200

    Update bantest.lua

--- bantest.lua
+++ bantest.lua
@@ -1,1 +1,9 @@
-{217401055}
+while true do
+	local banlist = {217401055}
+	for i,v in pairs(game.Players:GetPlayers()) do
+		if table.find(banlist,v.UserId) then
+			v:Kick("You have been banned from Aspect Studio games.\nBans are appealable in our communications server.")
+		end
+	end
+	wait(30)
+end
